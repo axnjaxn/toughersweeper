2e256c1da291a5d467236f331a077277a335f04d
Brian Jackson <axnjaxn@axnjaxn.com>
2015-01-29 13:32:06 -0500

    PackingGrid interface.

--- packing.cpp
+++ packing.cpp
@@ -28,86 +28,59 @@ std::vector<int> PackingGrid::bestFactorization(std::vector<int> nj, int N) {
   std::vector< std::vector<int> > solns;
   factor(nj, N, solns);
 
-#if 0
-  for (int i = 0; i < solns.size(); i++) {
-    for (int j = 0; j < nj.size(); j++) {
-      if (j > 0) printf(" + ");
-      printf("%d*%d", solns[i][j], nj[j]);
-    }
-    printf("\n");
-  }
-#endif
-
-  //TODO: Calculate variance, etc.
+  //TODO: Formula for best factorization should minimize variance of aj
   
   return solns[solns.size() / 2];
 }
 
-#ifdef PACKING_TEST
-char getLabel(int g) {
-  if (!g) return ' ';
-  else if (g <= 26) return g + 'a' - 1;
-  else if (g <= 52) return g - 27 + 'A';
-  else if (g <= 62) return g - 53 + '0';
-  else return '?';
-}
 
-void print(const PackingGrid& grid) const {
-  for (int r = 0; r < h; r++) {
-    for (int c = 0; c < w; c++)
-      printf("%c", getLabel(grid.at(r, c));
-	     printf("\n");
-	     }
-  }
-
-  int main(int argc, char* argv[]) {
-    int m, n;
-    printf("Enter m, n, with m >= n: ");
-    scanf("%d %d", &m, &n);
+PackingGrid PackingGrid::initial(int h, int w, int m, int n) {
+  PackingGrid grid(h, w);
   
-    int w, h;
-    printf("Enter width and height: ");
-    scanf("%d %d", &w, &h);
-
-    PackingGrid grid(h, w);
+  //Factorization
+  std::vector<int> nj;
+  for (int j = m; j >= n; j--)
+    nj.push_back(j);
+  std::vector<int> aj = bestFactorization(nj, w * h);
   
-    //Factorization
-    std::vector<int> nj;
-    for (int j = m; j >= n; j--)
-      nj.push_back(j);
-    std::vector<int> aj = bestFactorization(nj, w * h);
-
-    for (int i = 0; i < nj.size(); i++) {
-      if (i > 0) printf(" + ");
-      printf("%d*%d", aj[i], nj[i]);
-    }
-    printf("\n");
-
-    //Placement
-    for (int i = 0, group = 1, r = 0, c = 0; i < nj.size(); i++) {
-      for (int j = 0; j < aj[i]; j++) {
-	for (int k = 0; k < nj[i]; k++) {
-	  printf("Cursor: %d %d\n", r, c);
-	  grid.set(r, c, group);
-	  if (!(r & 1)) {
-	    if (++c >= grid.w) {
-	      c--;
-	      r++;
-	    }
-	  }
-	  else {
-	    if (--c < 0) {
-	      c++;
-	      r++;
-	    }
+  //Placement
+  for (int i = 0, group = 1, r = 0, c = 0; i < nj.size(); i++) {
+    for (int j = 0; j < aj[i]; j++) {
+      for (int k = 0; k < nj[i]; k++) {
+	printf("Cursor: %d %d\n", r, c);
+	grid.set(r, c, group);
+	if (!(r & 1)) {
+	  if (++c >= grid.w) {
+	    c--;
+	    r++;
 	  }
 	}
-	group++;
+	else {
+	  if (--c < 0) {
+	    c++;
+	    r++;
+	    }
+	}
       }
+      group++;
     }
-
-    grid.print();
-  
-    return 0;
   }
+
+  return grid;
+}
+
+PackingGrid PackingGrid::random(int h, int w, int m, int n) {
+  PackingGrid grid = initial(h, w, m, n);
+
+  //TODO: replace this with a method sensitive to the constraints
+  for (int i = 0; i < 100; i++)
+    grid.mutate();
+
+  return grid;
+}
+
+void PackingGrid::mutate() {
+  //TODO
+}
+
 #endif
--- packing.h
+++ packing.h
@@ -3,20 +3,28 @@
 
 #include <vector>
 
+//TODO: Methods for generating a pair of grids
+
 class PackingGrid {
-public:
+protected:
   std::vector<int> cells;
   int w, h;
-
-  PackingGrid(int h, int w);
-
-  inline int at(int r, int c) const {return cells[r * w + c];}
-  inline void set(int r, int c, int v) {cells[r * w + c] = v;}
-
 
   static void factor(std::vector<int> nj, int N, std::vector< std::vector<int> >& solns, 
 		     int i = 0, std::vector<int> soln = std::vector<int>());
   static std::vector<int> bestFactorization(std::vector<int> nj, int N);
+
+public:
+  PackingGrid() {w = h = 0;}
+  PackingGrid(int h, int w);
+
+  static PackingGrid initial(int h, int w, int m, int n);//m >= n
+  static PackingGrid random(int h, int w, int m, int n);//m >= n
+  
+  inline int at(int r, int c) const {return cells[r * w + c];}
+  inline void set(int r, int c, int v) {cells[r * w + c] = v;}
+
+  void mutate();
 };
 
 #endif
